a877cece7502466539552d34f6f9d7379105aa7a
devmoses <moses.thipparthi@yahoo.com>
2020-01-13 12:28:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 var config = require('./config/Config');
-var app = require('express')();
-var http = require('http');
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-var server = http.createServer(app);
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
+app.use(express.static(__dirname + '/public/'));
 
 server.listen(config.PORT, (err) => {
     if(err) { 
@@ -16,3 +14,29 @@ server.listen(config.PORT, (err) => {
         console.log(`Server listening on port ${config.PORT}`);
     }
 });
+
+app.get('/chat', (req, res) => {
+    res.sendFile(__dirname + '/public/views/messages.html');
+})
+
+io.on('connection', (socket) => {
+    console.log('New Client connected!', socket.id);
+
+    socket.on('chat_window', (message) => {
+        io.emit('chat_window', message);
+    });
+
+    socket.on('userTyping', (username) => {
+        io.emit('userTyping', username);
+    });
+
+    socket.on('userNotTyping', () => {
+        io.emit('userNotTyping');
+    });
+
+    socket.on('disconnect', (data) => {   
+        console.log(data);
+        console.log(`${data.username} disconnected`);
+    })
+});
+
